c54dc533197099803027b48196b1d89c5fdae3f9
ghoultf <ghoultf@163.com>
2019-02-26 11:10:07 +0800

    implement registration service

--- front-end/src/services/registration/index.js
+++ front-end/src/services/registration/index.js
@@ -1,7 +1,17 @@
+import axios from 'axios'
+
 export default {
+  /**
+   * Register a new user
+   * @param {Object} detail registration detail
+   */
   register (detail) {
     return new Promise((resolve, reject) => {
-      resolve()
+      axios.post('/registrations', detail).then(({ data }) => {
+        resolve(data)
+      }).catch((error) => {
+        reject(error)
+      })
     })
   }
 }
